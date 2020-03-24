c4775a1450d4281829454ce547eac8dfb9360b91
AnthonyTC89 <ptonyptc@gmail.com>
2020-03-24 15:04:02 -0500

    delete images from AWS S3 done, favicon added, error json added, pre-loader gif added

--- client/src/Dashboard/Images.js
+++ client/src/Dashboard/Images.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import axios from 'axios';
-import { uploadFile } from 'react-s3';
+import uuidv4 from 'uuid/v4';
+import { uploadFile, deleteFile } from 'react-s3';
 import { messages, images } from '../PageInfo.json';
-import 'bootstrap/dist/css/bootstrap-grid.css';
+import iconLoading from '../Images/pre-loader.gif';
+import 'bootstrap/dist/css/bootstrap.css';
+import './Images.css';
 
 class Images extends React.Component {
   constructor(props) {
@@ -10,8 +13,14 @@ class Images extends React.Component {
     this.state = {
       file: null,
       msg: 'mensaje',
-      uploading: false,
+      loading: false,
       allImages: images,
+    };
+    this.configS3 = {
+      bucketName: process.env.REACT_APP_S3_BUCKET,
+      region: process.env.REACT_APP_S3_BUCKET_REGION,
+      accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
     };
     this.handleChangeFile = this.handleChangeFile.bind(this);
     this.handleUploadS3 = this.handleUploadS3.bind(this);
@@ -22,22 +31,27 @@ class Images extends React.Component {
   }
 
   async getInfo() {
+    this.setState({
+      loading: true,
+    });
     try {
       const recipes = await axios.get('/api/recipes');
       this.setState({
+        loading: false,
         allImages: recipes.data,
       });
     } catch (err) {
       this.setState({
+        loading: false,
         msg: messages['006'],
       });
     }
   }
 
-  // eslint-disable-next-line class-methods-use-this
   handleChangeFile(e) {
     this.setState({
       file: e.currentTarget.files[0],
+      msg: '',
     });
   }
 
@@ -57,6 +71,7 @@ class Images extends React.Component {
       });
       return false;
     }
+    // if the file is not an image
     if (file.type.substring(0, 5) !== 'image') {
       this.setState({
         msg: messages['008'],
@@ -74,42 +89,75 @@ class Images extends React.Component {
       return;
     }
     this.setState({
-      uploading: true,
+      loading: true,
     });
-    const config = {
-      bucketName: process.env.REACT_APP_S3_BUCKET,
-      region: process.env.REACT_APP_S3_BUCKET_REGION,
-      accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
-    };
     try {
-      const data = await uploadFile(file, config);
+      const data = await uploadFile(file, this.configS3);
       const { bucket, key, location } = data;
-      await axios.post('/api/recipes', { bucket, key, location });
-      this.setState({
+      const res = await axios.post('/api/recipes', { bucket, key, location });
+      const newImage = { id: res.data.id, bucket, key, location };
+      this.setState((prevState) => ({
         msg: messages['003'],
-        uploading: false,
-      });
+        loading: false,
+        allImages: [...prevState.allImages, newImage],
+      }));
     } catch (err) {
       this.setState({
         msg: messages['004'],
-        uploading: false,
+        loading: false,
+      });
+    }
+  }
+
+  async handleDeleteFile(image) {
+    this.setState({
+      loading: true,
+    });
+    try {
+      await axios.delete(`api/recipes/${image.id}`);
+      await deleteFile(image.key, this.configS3);
+      this.setState((prevState) => ({
+        loading: false,
+        msg: messages['008'],
+        allImages: prevState.allImages.filter((item) => item.id !== image.id),
+      }));
+    } catch (err) {
+      this.setState({
+        loading: false,
+        msg: messages['009'],
       });
     }
   }
 
   render() {
-    const { uploading, msg, allImages } = this.state;
+    const { loading, msg, allImages } = this.state;
     const title = `Imagenes (${allImages.length})`;
     return (
       <section className="container">
         <h2>{title}</h2>
+        {loading
+          ? <img className="icon-loading" src={iconLoading} alt="icon-loading" />
+          : null}
         <form onSubmit={this.handleUploadS3}>
           <input type="file" accept="image/*" multiple={false} onChange={this.handleChangeFile} />
-          <button type="submit">Upload</button>
-          <p>{uploading}</p>
+          <button type="submit">Guardar</button>
           <p>{msg}</p>
         </form>
+        <div className="row">
+          {allImages.map((img) => (
+            <div key={uuidv4()} className="card border-success col-6 col-sm-4 col-md-3 col-lg-2">
+              <h6>{img.key}</h6>
+              <img className="card-img-top card-img" src={img.location} alt="hassana-file" />
+              <button
+                className="btn btn-danger"
+                type="button"
+                onClick={() => this.handleDeleteFile(img)}
+              >
+                Eliminar
+              </button>
+            </div>
+          ))}
+        </div>
       </section>
     );
   }
