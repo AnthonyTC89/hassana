dcd28da4831b0580a748ec325fbb9d769375e811
AnthonyTC89 <ptonyptc@gmail.com>
2020-04-03 10:04:46 -0500

    Social Networks added to Slogan

--- client/src/Components/Slogan.js
+++ client/src/Components/Slogan.js
@@ -1,35 +1,56 @@
 import React from 'react';
+import uuidv4 from 'uuid/v4';
+import axios from 'axios';
 import Info from '../PageInfo.json';
-import Icons from '../Icons.json';
+import iconLoading from '../Images/pre-loader.gif';
 import 'bootstrap/dist/css/bootstrap-grid.css';
 import './Slogan.css';
 
 class Slogan extends React.Component {
   constructor(props) {
     super(props);
-    const { headerTitle, headerText, headerRecipe, facebook } = Info;
+    const { headerTitle, headerText, headerRecipe } = Info;
     this.state = {
+      socialNetworks: [],
+      loading: false,
       headerTitle,
       headerText,
       headerRecipe,
-      facebook,
     };
   }
 
   componentDidMount() {
-    this.getInfo();
+    this.getSocialNetworks();
   }
 
   // eslint-disable-next-line class-methods-use-this
-  async getInfo() {
-    // eslint-disable-next-line no-console
-    console.log('getInfo Solgan');
+  async getSocialNetworks() {
+    this.setState({
+      loading: true,
+      message: '',
+    });
+    try {
+      const res = await axios.get('/api/social_networks');
+      this.setState({
+        socialNetworks: res.data,
+        loading: false,
+      });
+    } catch (err) {
+      this.setState({
+        loading: false,
+        message: 'Error en el Servidor',
+      });
+    }
   }
 
   render() {
-    const { headerTitle, headerText, headerRecipe, facebook } = this.state;
+    const { headerTitle, headerText, headerRecipe,
+      socialNetworks, loading, message } = this.state;
     return (
       <section className="container-fluid slogan-section" id="home">
+        {loading
+          ? <img className="icon-loading" src={iconLoading} alt="icon-loading" />
+          : null}
         <div className="row">
           <picture className="col-12 col-sm-6">
             <img className="slogan-image" src={headerRecipe} alt="hassana-masajes-salud" />
@@ -38,11 +59,15 @@ class Slogan extends React.Component {
             <h1>{headerTitle}</h1>
             <h3>{headerText}</h3>
             <div className="social-list">
-              {facebook === '' ? null : (
-                <a className="social-link" href={facebook}>
-                  <img className="social-icon" src={Icons.facebook} alt="facebook-icon" />
-                </a>
-              )}
+              <small>{message}</small>
+              {socialNetworks.map((item) => {
+                if (item.status === 0) { return null; }
+                return (
+                  <a key={uuidv4()} className="social-link" href={item.href}>
+                    <img className="social-icon" src={item.src} alt={`${item.name}-icon`} />
+                  </a>
+                );
+              })}
             </div>
           </div>
         </div>
