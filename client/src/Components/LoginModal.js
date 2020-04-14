bd57dbcc48d419d0c1d7ebda1980847a8e07dbc9
AnthonyTC89 <ptonyptc@gmail.com>
2020-04-14 15:55:27 -0500

    ModalLogin Updated

--- client/src/Components/LoginModal.js
+++ client/src/Components/LoginModal.js
@@ -1,23 +1,107 @@
 import React from 'react';
+import axios from 'axios';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Modal from 'react-awesome-modal';
-import Login from '../Containers/Login';
+import login from '../redux/actions/login';
+import { HTTPresponses } from '../PageInfo.json';
 import 'bootstrap/dist/css/bootstrap.css';
+import './LoginModal.css';
 
-const LoginModal = (props) => {
-  const { modalVisible, closeModal } = props;
-  const width = '50%';
-  const height = '25%';
-  return (
-    <Modal visible={modalVisible} width={width} height={height} onClickAway={() => closeModal()}>
-      <Login />
-    </Modal>
-  );
-};
+class LoginModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+      errMessage: '',
+      btnLoading: false,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.setState({
+      errMessage: '',
+      btnLoading: true,
+    });
+    const { username, password } = this.state;
+    const { addSession } = this.props;
+    const params = { username, password };
+    axios.post('login', params)
+      .then((res) => {
+        this.setState({
+          btnLoading: false,
+        });
+        const user = { id: res.data.id, username, status: res.data.status };
+        addSession(user);
+      })
+      .catch((err) => {
+        this.setState({
+          errMessage: `${HTTPresponses[err.response.status]}`,
+          btnLoading: false,
+        });
+      });
+  }
+
+  render() {
+    const { username, password, btnLoading, errMessage } = this.state;
+    const { modalVisible, closeModal } = this.props;
+    return (
+      <Modal visible={modalVisible} width="200" height="150" onClickAway={() => closeModal()}>
+        <div className="container-form">
+          <form className="login-form" onSubmit={!btnLoading ? this.handleSubmit : null}>
+            <input
+              className="form-control login-input"
+              onChange={this.handleChange}
+              type="text"
+              placeholder="usuario"
+              value={username}
+              name="username"
+              required
+            />
+            <input
+              className="form-control login-input"
+              onChange={this.handleChange}
+              type="password"
+              placeholder="contraseña"
+              value={password}
+              name="password"
+              required
+            />
+            <button className="btn btn-success login-btn" type="submit">
+              {btnLoading ? 'Espere...' : 'Ingresar'}
+            </button>
+            <small className="login-message">{errMessage}</small>
+          </form>
+        </div>
+      </Modal>
+    );
+  }
+}
 
 LoginModal.propTypes = {
   modalVisible: PropTypes.bool.isRequired,
   closeModal: PropTypes.func.isRequired,
+  addSession: PropTypes.func.isRequired,
 };
 
-export default LoginModal;
+const mapStateToProps = (state) => ({
+  session: state.session,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  addSession: (user) => dispatch(login(user)),
+});
+
+const LoginModalWrapper = connect(mapStateToProps, mapDispatchToProps)(LoginModal);
+
+export default LoginModalWrapper;
